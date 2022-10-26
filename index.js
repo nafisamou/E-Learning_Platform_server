a959949d2b30417f58d85a00d37e29a57006b132
nafisamou <108416863+nafisamou@users.noreply.github.com>
2022-10-26 15:09:30 +0600

    api updated

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ app.get("/course-categories", (req, res) => {
 });
 app.get("/category/:id", (req, res) => {
   const id = req.params.id;
-    const category_course = course.find((l) => l.category_id === id);
+    const category_course = course.filter((l) => l.category_id === id);
     res.send(category_course);
   
 });
 
-app.get("/course", (req, res) => {
+app.get("/courses", (req, res) => {
   res.send(course);
 });
 
-app.get("/course/:id", (req, res) => {
+app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
   const selectedLanguage = course.find((l) => l._id === id);
   res.send(selectedLanguage);
